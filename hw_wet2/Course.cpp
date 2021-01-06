866d0d7599500fb5ab7160a9392546d314173d45
BenHalpe <benhalpe@gmail.com>
2021-01-06 15:11:34 +0200

    updated Course for current assignment

--- hw_wet2/Course.cpp
+++ hw_wet2/Course.cpp
@@ -1,148 +1,25 @@
 #include "Course.h"
 
-Course::Course(int courseID, int numOfClasses){
-    this->courseID = courseID;
-    this->num_of_classes=numOfClasses;
-    try{
-        lecture_arr = new ListNode<Lecture>*[numOfClasses];
-        unwatched = List<Lecture>();
-        is_watched = new bool[numOfClasses];
-        for(int i = 0; i<numOfClasses; i++){
-            lecture_arr[i] = new ListNode<Lecture>(std::make_shared<Lecture>(courseID,i));
-            is_watched[i] = false;
-        }
-    }
-    catch(const std::exception& e){
-        throw e;
-    }
+Course::Course(int courseID):courseID(courseID),lecture_arr(5){}
 
-    for(int i = numOfClasses-1; i>=0; i--){
-        unwatched.add(lecture_arr[i]);
-    }
-
-    
-}
-Course::Course(const Course& copy){
-    courseID=copy.courseID;
-    unwatched= List<Lecture>();
-    num_of_classes=copy.num_of_classes;
-    lecture_arr = new ListNode<Lecture>*[copy.num_of_classes];
-    is_watched=new bool[num_of_classes];
-    
-    for(int i =0; i<num_of_classes;i++){
-        lecture_arr[i]= new ListNode<Lecture>();
-        *lecture_arr[i] = *copy.lecture_arr[i];
-        is_watched[i] = copy.is_watched[i];
-    }
-
-     for(int i = num_of_classes-1; i>=0; i--){
-        if(!is_watched[i]) {
-            unwatched.add(lecture_arr[i]);
-        }
-    }
-    
-}
-
-
-Course& Course::operator=(const Course& copy){
-
-    ListNode<Lecture>** lecture_arr_tmp = new ListNode<Lecture>*[copy.num_of_classes];
-    bool* is_watched_tmp=new bool[copy.num_of_classes];
-
-    try
-    {
-
-        for (int i = 0; i < copy.num_of_classes; i++)
-        {
-            lecture_arr_tmp[i] = new ListNode<Lecture>();
-            *lecture_arr_tmp[i] = *copy.lecture_arr[i];
-            is_watched_tmp[i] = copy.is_watched[i];
-        }
-
-
-        for (int i = 0; i < num_of_classes; i++)
-        {
-            if (is_watched[i])
-            {
-                delete lecture_arr[i];
-            }
-        }
-
-        delete[] lecture_arr;
-        delete[] is_watched;
-
-        courseID = copy.courseID;
-
-        num_of_classes = copy.num_of_classes;
-
-        lecture_arr = lecture_arr_tmp;
-        is_watched = is_watched_tmp;
-
-        unwatched.~List();
-        unwatched = List<Lecture>();
-
-        for (int i = num_of_classes - 1; i >= 0; i--)
-        {
-            if(!is_watched[i]){
-                unwatched.add(lecture_arr[i]);
-            }
-        }
-    }
-    catch (...)
-    {
-        for(int i=0;i<copy.num_of_classes;i++){
-            if(is_watched_tmp[i]){
-                delete lecture_arr_tmp[i];
-            }
-         }
-        delete[] lecture_arr_tmp;
-        delete[] is_watched_tmp;
-    }
-
-    return *this;
-}
-
-bool Course::get_watched(int classID) const{
-    return is_watched[classID];
-}
 
 int Course::get_id() const{
     return courseID;
 }
 
-void Course::set_watched(int classID){
-    is_watched[classID] = true;
-}
 
 int Course::getNumOfClasses(){
-    return num_of_classes;
+    return lecture_arr.getCounter();
 
 }
-ListNode<Lecture>** Course::getLectureArray(){
+
+DynamicArray<Lecture>& Course::getLectureArray(){
     return lecture_arr;
 }
 
-ListNode<Lecture>*  Course::getUnwatchedRoot() const{
-    return unwatched.getRoot();
-}
-
-void Course::setUnwatchedRoot(ListNode<Lecture>* new_root) {
-    unwatched.setRoot(new_root);
-}
 
 Lecture& Course::getLecture(int classID){
-    return *lecture_arr[classID]->getValue();
+    return lecture_arr[classID];
 }
 
-Course::~Course(){
-    
-    for(int i=0;i<num_of_classes;i++){
-        if(is_watched[i]){
-            delete lecture_arr[i];
-        }
-    }
-    
-    delete[] lecture_arr;
-    delete[] is_watched;
-}
 
--- hw_wet2/Course.h
+++ hw_wet2/Course.h
@@ -2,38 +2,30 @@
 #define _COURSE_H_
 
 #include <exception>
-#include "List.h"
-#include "types.h"
+#include "DynamicArray.h"
 #include "Lecture.h"
 class Course{
     
     int courseID;
-    //Lecture* unwatched_arr;
-    ListNode<Lecture>** lecture_arr;
-    List<Lecture> unwatched;
-    bool* is_watched;
-    int num_of_classes;
+    DynamicArray<Lecture> lecture_arr;
+    
 
 
     public:
-        Course(int courseID,int numOfClasses);
-        Course(const Course& copy);
-        Course& operator=(const Course& copy);
-        ListNode<Lecture>**  getLectureArray();
-    
+        Course(int courseID);
+        Course(const Course& copy) = default;
+        Course& operator=(const Course& copy) = default;
+
+        /*
+        * returns reference to the lecture dynamic array
+        */
+        DynamicArray<Lecture>&  getLectureArray();
+
+        /*
+        * returns reference to specific lecture by id
+        */
         Lecture& getLecture(int classID);
-        /*
-        * returns wether the class with the given ID has been watched already
-        */
-        bool get_watched(int classID) const;
-        /*
-        *returns a ponter to the root of the list of the unwatched courses
-        */
-        ListNode<Lecture>*  getUnwatchedRoot() const;
-        /*
-        *sets the root of the Unwatched list to be 'new_root'
-        */
-        void setUnwatchedRoot(ListNode<Lecture>* new_root) ;
+    
         /*
         * returns course ID
         */
@@ -42,17 +34,7 @@ class Course{
         * returns number of classes
         */
         int getNumOfClasses();
-        /*
-        * mark classID as a watched class in the is_watched array
-        */
-        void set_watched(int classID);
-        /*
-        * ???
-        */
-        void reset_unwatched();
 
-
-        
 
         ~Course();
 
